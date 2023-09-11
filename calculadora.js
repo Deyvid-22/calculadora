b91551594f8df86141f4bf795355bc958c5cea6e
DEYVID SILVA <140274792+Deyvid-22@users.noreply.github.com>
2023-09-11 15:07:46 -0300

    ajustes

---
+++ calculadora.js
@@ -0,0 +1,91 @@
+let input = document.getElementById("input");
+let button = document.querySelectorAll(".adicionar");
+let numero = document.querySelectorAll(".num");
+let apagar = document.querySelector(".icon");
+let reset = document.querySelector(".reset");
+let add = document.querySelector('.add')
+let result = document.querySelector('.result')
+let addSinal = document.querySelector('.add_sinal')
+num = false;
+
+numero.forEach((numero) => {
+  numero.addEventListener("click", clicar);
+  function clicar() {
+    input.value = input.value + numero.innerHTML;
+    num = true;
+  }
+});
+
+button.forEach((button) => {
+  button.addEventListener("click", clicar_num);
+  function clicar_num() {
+    if (input.value === "") {
+      input.value = "";
+    } else if (num === true) {
+      input.value = input.value + button.innerHTML;
+      num = false;
+    } else {
+      texto = input.value;
+      palavra = texto.split("");
+      palavra.pop();
+      novapalavra = palavra.join("");
+      input.value = novapalavra;
+      input.value = input.value + button.innerHTML;
+    }
+  }
+});
+
+addSinal.addEventListener('click',add_sinal)
+function add_sinal(){
+     virgula = input.value
+     if(virgula.includes(',') == false){
+        input.value = input.value + ','
+     }else{
+      alert('ação indisponivel')
+     }
+       
+    // console.log(virgulaArray.includes(','))
+     
+}
+
+
+
+add.addEventListener('click', adicionar)
+
+function adicionar() {
+
+  if (num === false) {
+    input.value = input.value + '('
+    
+  } else if (num === true) {
+    input.value = input.value + ')'
+    
+    //alterar a logica
+  }
+
+}
+
+apagar.addEventListener("click", apaga);
+function apaga() {
+  if (input.value.length > 0) {
+    input.value = input.value.slice(0, -1);
+  }
+}
+reset.addEventListener("click", resetar);
+function resetar() {
+  input.value = "";
+}
+
+result.addEventListener('click', resultado)
+function resultado() {
+  result = input.value
+  novoValor = result.replace(/x/g, '*')
+  valorPorce = novoValor.replace(/%/g, '/100*')
+  valorFinal = valorPorce.replace(/÷/g, '/')
+  input.value = eval(valorFinal)
+}
+
+
+
+
+
